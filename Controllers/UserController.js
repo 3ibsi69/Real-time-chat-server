cc086c7e47bb7c873efeab58a4adec7aa0e114e3
3ibsi69 <a.abbassi21306@pi.tn>
2023-08-07 18:58:51 +0100

    added login controller

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,5 +1,6 @@
 var User=require("../Modules/User");
 const bcrypt=require("bcrypt");
+const e = require("express");
 const jwt=require("jsonwebtoken");
 
 const signup=async(req,res)=>{
@@ -22,3 +23,21 @@ const signup=async(req,res)=>{
         });
     });
 }
+
+const login=async(req,res)=>{
+    var user=await User.findOne({email:req.body.email});
+    if(user){
+        bcrypt.compare(req.body.password,user.password,(err,result)=>{
+            if(result){
+                var token=jwt.sign({id:user._id},"c21");    
+                res.send([token]);
+            }
+            else{
+                res.send({msg:"Wrong Password"});
+            }
+        });
+        }else{
+            res.send({msg:"Wrong Email"});
+        }
+    };
+        

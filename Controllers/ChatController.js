b1d4d1762c82a2e61fbc97581f23cda4bcf99800
3ibsi69 <a.abbassi21306@pi.tn>
2023-08-09 10:52:02 +0100

    added the controllers

---
+++ Controllers/ChatController.js
@@ -0,0 +1,60 @@
+const Chat = require('..//Modules/Chat');
+
+
+const creatChat=async(req,res)=>{
+    const {firstId,secondId}=req.body;
+
+    try{
+        const chat=await Chat.findOne({
+            members:{$all:[firstId,secondId]}
+        })
+        if(chat) res.status(200).json(chat);
+
+        const newChat=new Chat({
+            members:[firstId,secondId];
+        })
+
+        const response=await newChat.save();
+        res.status(200).json(response);
+
+    }catch(err){
+        console.log(err);
+        res.status(500).send({msg:"Server Error"});
+    }
+
+}
+
+const findUserChats=async(req,res)=>{
+    const userId=req.params.userId;
+    try{
+        const chats=await Chat.find({
+            members:{$in:[userId]}
+        })
+        res.status(200).json(chats);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send({msg:"Server Error"});
+    }
+}
+
+const findChat=async(req,res)=>{
+    const {firstId,secondId}=req.params;
+   
+    try{
+        const chat=await Chat.findOne({
+            members:{$all:[firstId,secondId]}
+        })
+        res.status(200).json(chat);
+}
+catch(err){
+    console.log(err);
+    res.status(500).send({msg:"Server Error"});
+}
+}
+
+module.exports={
+    creatChat,
+    findUserChats,
+    findChat
+}

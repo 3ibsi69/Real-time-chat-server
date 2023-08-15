3a191dcbc7e8040c4d41caa44be2940c51b9f188
3ibsi69 <a.abbassi21306@pi.tn>
2023-08-15 21:02:47 +0100

    added message controller

---
+++ Controllers/MessageController.js
@@ -0,0 +1,36 @@
+const messageModel = require("..//Modules/Messaege");
+
+// create Message
+
+const createMessage = async (req, res) => {
+  const { chatId, senderId, text } = req.body;
+  const message = new messageModel({
+    chatId,
+    senderId,
+    text,
+  });
+  try {
+    await message.save();
+    res.status(200).json({ message: "message created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "message not created" });
+  }
+};
+
+// get all messages
+
+const getmessages = async (req, res) => {
+  const { chatId } = req.params;
+
+  try {
+    const messages = await messageModel.find({ chatId });
+    res.status(200).json({ messages });
+  } catch (error) {
+    res.status(500).json({ message: "messages not found" });
+  }
+};
+
+module.children = {
+  createMessage,
+    getmessages,
+};
--- Modules/Messaege.js
+++ Modules/Messaege.js
@@ -1,10 +1,14 @@
-const mongoose = require('./Connection');
+const mongoose = require("./Connection");
 
+const messageSchema = new mongoose.Schema(
+  {
+    chatId: String,
+    senderId: String,
+    ext: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const messageSchema = new mongoose.Schema({
-    chatId:String,
-    senderId:String,
-    ext:String
-});
-
-const Message = mongoose.model('Message',messageSchema);
+const Message = mongoose.model("Message", messageSchema);
